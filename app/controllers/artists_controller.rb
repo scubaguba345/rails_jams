50f6035423ece58e438ee0a6fb7eaea22099ab23
shiro-han <shan99business@gmail.com>
2020-06-24 15:48:09 -0400

    update CRUD complete

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -18,15 +18,18 @@ class ArtistsController < ApplicationController
     end
     
     def edit
-
+        @artist = Artist.find(params[:id])
     end
 
     def update
-
+        @artist = Artist.find(params[:id])
+        @artist.update(artist_params)
+        
+        redirect_to artist_path(@artist)
     end
 
     def artist_params
-       params.require(:artist).permit(:name, :age, :bio) 
+        params.require(:artist).permit(:name, :age, :bio) 
     end
 
 
